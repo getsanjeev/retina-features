ea9f19862fffaa02268c2bc312ebf2483568d99e
getsanjeev <getsanjeevdubey@gmail.com>
2017-04-05 14:22:54 +0530

    added improvement

--- MODEL_1_SVM/bv_extract.py
+++ MODEL_1_SVM/bv_extract.py
@@ -4,7 +4,7 @@ import scipy.misc
 
 images = ['1.png', '2.png',  '3.png','4.png','5.png','6.png','7.png','8.png','9.png','10.png']
 name = ''
-counter = 1;
+counter = 11;
 for image in images:
 	fundus = cv2.imread(image)
 	dim = (800,700)
@@ -20,6 +20,8 @@ for image in images:
 	#edge_fundus_green = cv2.Canny(green_fundus, 10, 100)
 	#cv2.imshow("edge green",edge_fundus_green)
 	gray_fundus = cv2.cvtColor(fundus, cv2.COLOR_BGR2GRAY)
+	impo = cv2.Canny(gray_fundus, 5, 50)
+	cv2.imshow("IMPORTANt",impo)
 	#cv2.imshow("gray_fundus",gray_fundus)
 	#cv2.imshow("green_fundus",green_fundus)
 	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
@@ -44,8 +46,8 @@ for image in images:
 	#upper2 = int(min(255, (1.0 + sigma) * v2))
 	edge_fundus = cv2.Canny(non_smooth_fundus, 5, 50)
 	edge_fundus2 = cv2.Canny(smooth_fundus, 5, 50)
-	#cv2.imshow("edge smooth fundus",edge_fundus2)
-	#cv2.imshow("edge nonsmooth fundus",edge_fundus)
+	cv2.imshow("edge smooth fundus",edge_fundus2)
+	cv2.imshow("edge nonsmooth fundus",edge_fundus)
 
 	blood_vessels = cv2.addWeighted(edge_fundus2,0.6,contrast_enhanced_inverted_green_fundus,0.4,0)
 	#dst = cv2.addWeighted(img1,0.7,img2,0.3,0)
@@ -53,7 +55,9 @@ for image in images:
 	#cv2.imshow("blood vessels", blood_vessels)
 	#binary_blood_vessel1 = cv2.adaptiveThreshold(non_smooth_fundus,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,21,2)
 	binary_blood_vessel2 = cv2.adaptiveThreshold(smooth_fundus,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,21,2)
-	cv2.imshow("blood vessels adaptive smooth", binary_blood_vessel2)
+	im2, contours, hierarchy = cv2.findContours(binary_blood_vessel2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	cv2.imshow("most imp",binary_blood_vessel2)
+	#cv2.imshow("blood vessels adaptive smooth", binary_blood_vessel2)
 	name = str(counter) + '.jpg'
 	scipy.misc.imsave(name, binary_blood_vessel2)
 	counter = counter +1;
--- MODEL_1_SVM/gabor.py
+++ MODEL_1_SVM/gabor.py
@@ -4,7 +4,7 @@ import cv2
 image = cv2.imread("fundus.png")
 gray_fundus = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("gray",gray_fundus)
-g_kernel = cv2.getGaborKernel((51,51), 1.0, 150,20.0, 0.5, 0, ktype=cv2.CV_32F)
+g_kernel = cv2.getGaborKernel((51,51), 6.0, 150, 20.0, 0.5, 0, ktype=cv2.CV_32F)
 filtered_img1 = cv2.filter2D(gray_fundus, cv2.CV_8UC3, g_kernel)
 cv2.imshow("blood vessels", filtered_img1)
 cv2.waitKey(0)
