8134fa5f724f8a1398a242b5e03a6f6ea3122493
getsanjeev <getsanjeevdubey@gmail.com>
2017-04-12 03:24:53 +0530

    corrected (improved) BV extraction

--- MODEL_1_SVM/feature_extraction/extract_bv.py
+++ MODEL_1_SVM/feature_extraction/extract_bv.py
@@ -27,11 +27,15 @@ founter = 11;
 
 for image in images:
 	fundus = cv2.imread(image)	
+
+	# applying contrast enhancement & preprocesing stuff
 	dim = (800,700)
-	fundus = cv2.resize(fundus, dim, interpolation = cv2.INTER_AREA)
+	fundus = cv2.resize(fundus, dim, interpolation = cv2.INTER_AREA)	
 	b,green_fundus,r = cv2.split(fundus)
 	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 	contrast_enhanced_green_fundus = clahe.apply(green_fundus)
+
+	# applying alternate sequential filtering (3 times closing opening)
 	r1 = cv2.morphologyEx(contrast_enhanced_green_fundus, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)), iterations = 1)
 	R1 = cv2.morphologyEx(r1, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)), iterations = 1)
 	r2 = cv2.morphologyEx(R1, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11)), iterations = 1)
@@ -39,41 +43,42 @@ for image in images:
 	r3 = cv2.morphologyEx(R2, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(23,23)), iterations = 1)
 	R3 = cv2.morphologyEx(r3, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(23,23)), iterations = 1)	
 	f4 = cv2.subtract(R3,contrast_enhanced_green_fundus)
+	f5 = clahe.apply(f4)		
 
-	f5 = clahe.apply(f4)
-
-	ret,f6 = cv2.threshold(f5,15,255,cv2.THRESH_BINARY)
-	mask = np.ones(f5.shape[:2], dtype="uint8") * 255
-	im2, contours, hierarchy = cv2.findContours(f6,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+	# removing very small contours through are parameter noise removal
+	ret,f6 = cv2.threshold(f5,15,255,cv2.THRESH_BINARY)	
+	mask = np.ones(f5.shape[:2], dtype="uint8") * 255	
+	im2, contours, hierarchy = cv2.findContours(f6.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 	for cnt in contours:
 		if cv2.contourArea(cnt) <= 200:
-			cv2.drawContours(mask, [cnt], -1, 0, -1)
-			cv2.imshow("mask",mask)
+			cv2.drawContours(mask, [cnt], -1, 0, -1)			
 	im = cv2.bitwise_and(f5, f5, mask=mask)
-	ret,fin = cv2.threshold(im,15,255,cv2.THRESH_BINARY_INV)	
-	fundus_eroded = cv2.erode(fin, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3)), iterations=1)
-	#remove_white_spots(fundus_eroded,founter)
+	ret,fin = cv2.threshold(im,15,255,cv2.THRESH_BINARY_INV)			
+	newfin = cv2.erode(fin, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3)), iterations=1)		
+	
 
-
+	# removing blobs of microaneurysm & unwanted bigger chunks taking in consideration they are not straight lines like blood
+	#vessels and also in an interval of area
+	fundus_eroded = cv2.bitwise_not(newfin)	
 	xmask = np.ones(fundus.shape[:2], dtype="uint8") * 255
-	x1, xcontours, xhierarchy = cv2.findContours(fin,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)	
+	x1, xcontours, xhierarchy = cv2.findContours(fundus_eroded.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)	
 	for cnt in xcontours:
 		shape = "unidentified"
 		peri = cv2.arcLength(cnt, True)
 		approx = cv2.approxPolyDP(cnt, 0.04 * peri, False)   				
-		if len(approx) > 5 and cv2.contourArea(cnt) <= 2000 and cv2.contourArea(cnt) >= 200:
+		if len(approx) > 4 and cv2.contourArea(cnt) <= 3000 and cv2.contourArea(cnt) >= 100:
 			shape = "circle"	
 		else:
 			shape = "veins"
 		if(shape=="circle"):
-			cv2.drawContours(xmask, [cnt], -1, 0, -1)
-			
-	#im_fin = cv2.bitwise_and(fundus_eroded, fundus_eroded, mask=xmask)
-
+			cv2.drawContours(xmask, [cnt], -1, 0, -1)	
+	
+	finimage = cv2.bitwise_and(fundus_eroded,fundus_eroded,mask=xmask)	
+	blood_vessels = cv2.bitwise_not(finimage)	
 	name = str(counter) + '.jpg'
 	name2 = str(founter) + '.jpg'
-	scipy.misc.imsave(name,xmask)
-	scipy.misc.imsave(name2,fundus_eroded)
+	scipy.misc.imsave(name,blood_vessels)
+	scipy.misc.imsave(name2, newfin )
 	counter = counter +1;
 	founter = founter +1;
 
